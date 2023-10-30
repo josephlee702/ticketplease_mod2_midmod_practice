90a4d5b2a36e929d546f663ca6c2c06c3cf84dcb
Joseph Lee <jhjlee702@gmail.com>
2023-10-30 01:10:11 -0600

    finish user story 3

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Employee, type: :model do
     @ticket1 = @jack.tickets.create!(subject: "random task 1", age: 5)
     @ticket2 = @jack.tickets.create!(subject: "random task 2", age: 3)
     @ticket3 = @jack.tickets.create!(subject: "random task 3", age: 2)
+    @ticket4 = Ticket.create!(subject: "email boss", age: 7)
   end
   
   describe "relationships" do
@@ -26,6 +27,21 @@ RSpec.describe Employee, type: :model do
   describe "#oldest_ticket" do
     it "returns the oldest ticket that an employee has" do
       expect(@jack.oldest_ticket).to eq(@ticket1.subject)
+    end
+  end
+
+  describe "#add_ticket" do
+    it "adds a ticket to the employee" do
+      expect(@jack.tickets).to eq([@ticket1, @ticket2, @ticket3])
+      @jack.add_ticket(@ticket4)
+      expect(@jack.tickets).to eq([@ticket1, @ticket2, @ticket3, @ticket4])
+    end
+  end
+
+  describe "#order_tickets" do
+    it "orders tickets from oldest to newest" do
+      @jack.add_ticket(@ticket4)
+      expect(@jack.order_tickets).to eq([@ticket4, @ticket1, @ticket2, @ticket3])
     end
   end
 end
